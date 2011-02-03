9f91e38b2c735978863edffb944a8cba3ecad43b
Alexander Willner <alex@willner.ws>
2011-02-03 18:02:55 +0100

    Added missing twitter secrets template

---
+++ src/TwitterSecrets.h
@@ -0,0 +1,11 @@
+/*
+ *  TwitterSecrets.h
+ *  Pomodoro
+ *
+ *  Created by Alexander Willner on 03.02.11.
+ *  Copyright 2011 University of Bonn. All rights reserved.
+ *
+ */
+
+#define _consumerkey @"" 
+#define _secretkey @""
